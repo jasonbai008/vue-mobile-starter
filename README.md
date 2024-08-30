fc9aeebdf5dd4cefbdb203108da5341e8a189561
JasonBai008 <15810683857@163.com>
2024-08-30 13:45:37 +0800

    更新说明

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# temp
+# vue2-vant2-px2vw-starter
 
 ## Project setup
 ```
@@ -15,5 +15,16 @@ npm run serve
 npm run build
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Tech Stack
+
+- vue 2.x
+- vue-cli 5.x
+- vant 2.x
+- postcss-px-to-viewport
+- vuex
+- vue-router
+
+## Notes
+
+1. 默认设计稿宽度：750px，Vant组件库宽度：375px，已做适配。文章参考：https://blog.csdn.net/qq_39831708/article/details/125472960
+2. Vant 2 官方文档：https://vant-ui.github.io/vant/v2/#/zh-CN/
--- vue.config.js
+++ vue.config.js
@@ -8,26 +8,23 @@ module.exports = defineConfig({
           return {
             plugins: [
               // ["autoprefixer"],
-              // vant px转vw。参坑：单独写在postcss.config.js中无法解析vant内部样式
               {
                 "postcss-px-to-viewport": {
-                  unitToConvert: "px",
-                  // 区分vant设计以375为基准。实际项目ui为750的情况
-                  viewportWidth: loaderContext.resourcePath.includes("vant")
-                    ? 375
-                    : 750,
-                  unitPrecision: 5,
-                  propList: ["*"],
-                  viewportUnit: "vw",
-                  fontViewportUnit: "vw",
-                  selectorBlackList: [],
-                  minPixelValue: 1,
-                  mediaQuery: false,
-                  replace: true,
-                  exclude: [],
-                  landscape: false,
-                  landscapeUnit: "vw",
-                  landscapeWidth: 568,
+                  unitToConvert: "px", // 需要转换的单位，默认为"px"                  
+                  viewportWidth: loaderContext.resourcePath.includes("vant") ? 375 : 750, // 区分vant设计以375为基准。实际项目ui为750的情况
+                  unitPrecision: 5, // 单位转换后保留的精度
+                  propList: ["*"], // 能转化为vw的属性列表,!font-size表示font-size后面的单位不会被转换
+                  viewportUnit: "vw", // 希望使用的视口单位
+                  fontViewportUnit: "vw", // 字体使用的视口单位
+                  selectorBlackList: ["keep-px"], // 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。
+                  minPixelValue: 1, // 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
+                  mediaQuery: false, // 媒体查询里的单位是否需要转换单位
+                  replace: true, //  是否直接更换属性值，而不添加备用属性
+                  exclude: [], // 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
+                  include: [/src/], // 如果设置了include，那将只有匹配到的文件才会被转换
+                  landscape: false, // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
+                  landscapeUnit: "vw", // 横屏时使用的单位
+                  landscapeWidth: 1338, // 横屏时使用的视口宽度
                 },
               },
             ],
